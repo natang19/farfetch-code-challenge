9316343ee9b7f29f0394d58977a8cbb0e607baf4
Natan Cicero <natan.cicero@thetrainline.com>
2026-02-16 13:57:07 +0000

    Add scalar for api documentation

--- src/Inventory.Management.Api/Controllers/CategoryController.cs
+++ src/Inventory.Management.Api/Controllers/CategoryController.cs
@@ -2,11 +2,18 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Inventory.Management.Api.Controllers;
 
+/// <summary>
+/// 
+/// </summary>
 [ApiController]
 [Route("api/category")]
 public class CategoryController : ControllerBase
 {
 
+    /// <summary>
+    /// Gell all categories
+    /// </summary>
+    /// <returns></returns>
     [HttpGet(Name = "GetCategories")]
     public ActionResult<IEnumerable<WeatherForecast>> Get()
     {
--- src/Inventory.Management.Api/Program.cs
+++ src/Inventory.Management.Api/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +14,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference("documentation");
+    // Automatically redirect to Scalar documentation
+    app.MapGet("/", () => Results.Redirect("/documentation"));
 }
 
 app.UseHttpsRedirection();
